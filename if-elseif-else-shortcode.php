2d7cae5ad778a6c3a7b007e33780724926e65b02
Nathan Kowald <nathankowald@gmail.com>
2026-07-04 23:02:13 +0930

    0.3.0 - Fixes and improvements

--- if-elseif-else-shortcode.php
+++ if-elseif-else-shortcode.php
@@ -7,7 +7,9 @@
  * Author URI:      https://www.nathankowald.com
  * Text Domain:     if-elseif-else-shortcode
  * Domain Path:     /languages
- * Version:         0.2.0
+ * Version:         0.3.0
+ * Requires at least: 4.5
+ * Requires PHP:    8.0
  *
  * @package         If_Elseif_Else_Shortcode
  */
@@ -16,52 +18,67 @@
  * Inspired by: https://level7systems.co.uk/wordpress-shortcodes-else-statement
  */
 
-// Register shortcode.
-add_shortcode( 'if', 'iee_if_elseif_else_statement' );
+// Prevent direct file access.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Register shortcode. The tag is filterable to avoid clashes with other plugins.
+add_shortcode( apply_filters( 'if_elseif_else_shortcode_tag', 'if' ), 'iee_if_elseif_else_statement' );
 
 /**
- * @param $atts
- * @param $content
+ * Renders the matching if / elseif / else branch of the shortcode content.
+ *
+ * @param array|string $atts    Shortcode attributes. First value is the callable.
+ * @param string|null  $content Enclosed shortcode content.
  *
  * @return string
  */
-function iee_if_elseif_else_statement( $atts, $content ) {
-	if ( empty( $atts ) ) {
+function iee_if_elseif_else_statement( $atts = array(), $content = null ) {
+	if ( empty( $atts ) || null === $content ) {
 		return '';
 	}
 
 	$pattern_else      = '[else]';
 	$pattern_elseif    = "/\[elseif\s([a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff:]*)\s?([^\[\]]*)?\]/";
 	$callable          = array_shift( $atts );
-	$content_if        = $content;
 	$content_else      = '';
 	$content_elseif    = '';
 	$elseif            = false;
 	$elseif_true_index = 0;
 
 	if ( ! iee_is_valid_callable( $callable ) ) {
-		return __( 'If shortcode error: [if] argument must be callable', 'if-elseif-else-shortcode' );
+		return esc_html__( 'If shortcode error: [if] argument must be callable', 'if-elseif-else-shortcode' );
 	}
 
-	$if = (boolean) call_user_func_array( $callable, $atts );
+	// array_values() drops any string keys so named attributes are not passed
+	// as PHP 8 named arguments (which would trigger a fatal error).
+	$if = (bool) call_user_func_array( $callable, array_values( $atts ) );
+
+	// Separate the [else] branch up-front so all if/elseif parsing operates on
+	// the same string and index counts stay aligned.
+	$content_conditions = $content;
+	if ( str_contains( $content, $pattern_else ) ) {
+		list( $content_conditions, $content_else ) = explode( $pattern_else, $content, 2 );
+	}
 
 	// If condition is false: check elseif condition(s).
 	if ( ! $if &&
-	     preg_match_all(
-		     $pattern_elseif,
-		     $content,
-		     $matches,
-		     PREG_SET_ORDER
-	     ) > 0
+		preg_match_all(
+			$pattern_elseif,
+			$content_conditions,
+			$matches,
+			PREG_SET_ORDER
+		) > 0
 	) {
 		foreach ( $matches as $match ) {
-			$callable = array_values( array_slice( $match, - 2 ) )[0];
+			$callable = $match[1];
 			if ( ! iee_is_valid_callable( $callable ) ) {
-				return __( 'If shortcode error: [elseif] argument must be callable', 'if-elseif-else-shortcode' );
+				return esc_html__( 'If shortcode error: [elseif] argument must be callable', 'if-elseif-else-shortcode' );
 			}
-			$atts            = array_values( array_slice( $match, - 1 ) )[0];
-			$callable_params = empty( $atts ) ? [] : explode( ' ', $atts );
-			$elseif          = (boolean) call_user_func_array( $callable, $callable_params );
+			$params          = isset( $match[2] ) ? trim( $match[2] ) : '';
+			$callable_params = ( '' === $params ) ? array() : preg_split( '/\s+/', $params );
+			$elseif          = (bool) call_user_func_array( $callable, $callable_params );
 			if ( $elseif ) {
 				// elseif condition is true: no need to check further.
 				break;
@@ -69,14 +86,10 @@ function iee_if_elseif_else_statement( $atts, $content ) {
 			$elseif_true_index ++;
 		}
 	}
-	if ( strpos( $content, $pattern_else ) !== false ) {
-		list( $content_if, $content_else ) = explode( $pattern_else, $content, 2 );
-	}
-	$contents = preg_split( $pattern_elseif, $content_if );
-	if ( $contents !== false ) {
-		$content_if = array_shift( $contents );
-	}
-	if ( $elseif ) {
+
+	$contents   = preg_split( $pattern_elseif, $content_conditions );
+	$content_if = ( false !== $contents ) ? array_shift( $contents ) : $content_conditions;
+	if ( $elseif && isset( $contents[ $elseif_true_index ] ) ) {
 		$content_elseif = $contents[ $elseif_true_index ];
 	}
 
@@ -86,18 +99,18 @@ function iee_if_elseif_else_statement( $atts, $content ) {
 /**
  * Checks to see if the arguments passed to [if] and [elseif] are callable.
  *
- * @param $callable
+ * @param string $callable Callable name from the shortcode.
  *
  * @return bool
  */
 function iee_is_valid_callable( $callable ) {
-	return is_callable( $callable ) && in_array( $callable, iee_get_allowed_callables() );
+	return is_callable( $callable ) && in_array( $callable, iee_get_allowed_callables(), true );
 }
 
 /**
  * List of allowed, filterable callables.
  *
- * @return mixed|void
+ * @return array
  */
 function iee_get_allowed_callables() {
 	$whitelist = [
